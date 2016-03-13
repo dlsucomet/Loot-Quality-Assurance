f030e52665d15260007d1f1fc4a394c42a3884ea
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-03-13 22:24:57 +0800

    Loot Beta v0.1.5
    - Added SplashScreenActivity -

---
+++ app/src/main/java/tapales/manto/bhuller/loot/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package tapales.manto.bhuller.loot;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SplashScreenActivity extends AppCompatActivity{
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    public void onCreate(Bundle icicle){
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
