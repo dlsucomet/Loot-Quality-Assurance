6283a510168a7bcaf7ad4d106925f6a6b7e59933
Migo Andres Dancel <Migo Andres Dancel>
2016-07-02 13:57:03 +0800

    Removed logout

--- app/src/main/java/tapales/manto/bhuller/loot/MainActivity.java
+++ app/src/main/java/tapales/manto/bhuller/loot/MainActivity.java
@@ -66,9 +66,6 @@ public class MainActivity extends AppCompatActivity{
                 DialogFragment df = new AboutDialogFragment();
                 df.show(getFragmentManager(), "");
                 return true;
-            case R.id.menu_logout:
-                finish();
-                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
